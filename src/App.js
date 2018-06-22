6341de9e6eaae8ccb336c3eae0cb6a98973e6671
Fabiano Martins <fabiano.martins@visagio.com>
2018-06-22 01:16:11 -0300

    Adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ProductTable from './ProductTable';
 import ProductForm from './ProductForm';
 
-class App extends Component {
+export default class App extends Component {
 
   constructor(props){
     super(props);
@@ -27,17 +27,45 @@ class App extends Component {
   }
 
   render() {
+
+    const ListProducts = (props) => {
+        return (
+          <ProductTable listProducts={this.state.listProducts} deleteProduct={this.deleteProduct}/>
+        );
+    };
+
+    const FormProducts = (props) => {
+        return (
+          <ProductForm createProduct={this.createProduct}/>
+        );
+    };
+
     return (
-      <div className="App">
-        <h1>Lista de Produtos </h1>
-        <ProductTable listProducts={this.state.listProducts} deleteProduct={this.deleteProduct}/>
-        <br />
-        <hr />
-        <br />
-        <ProductForm createProduct={this.createProduct}/>
-      </div>
+
+      <Router>
+        <div>
+                  <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <a class="navbar-brand" href="#">Produtcs</a>
+              </div>
+
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li><Link to={'/'}>List of Products</Link></li>
+                  <li><Link to={'/form'}>Insert a product</Link></li>
+                </ul>
+              </div>
+            </div>
+          </nav>          
+          <Switch>
+            <Route exact path='/' component={ListProducts} />
+            <Route exact path='/form' component={FormProducts} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+
